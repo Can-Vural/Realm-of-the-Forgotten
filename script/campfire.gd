0ef867620ef29d6d49002dbee1771b7a788969fb
Can Vural <66164392+Can-Vural@users.noreply.github.com>
2025-05-30 14:32:15 +0300

    add code

--- script/campfire.gd
+++ script/campfire.gd
@@ -1,9 +1,32 @@
 extends CharacterBody2D
 
+var IS_PLAYER_IN_AREA = false
+var IS_AVAILABLE = true
 
-func _on_hitbox_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+
+func _ready():
+	$AnimatedSprite2D.play("campfire_animation")
+
+func _physics_process(delta):
+	increase_player_hp()
 
 
-func _on_hitbox_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+func _on_hitbox_body_entered(body):
+	if body.has_method("player"):
+		IS_PLAYER_IN_AREA = true
+
+func _on_hitbox_body_exited(body):
+	if body.has_method("player"):
+		IS_PLAYER_IN_AREA = false
+
+func increase_player_hp():
+	if IS_PLAYER_IN_AREA:
+		if IS_AVAILABLE:
+			global.PLAYER_HEALTH += 15
+			
+			$increase_hp_cooldown.start()
+			IS_AVAILABLE = false
+
+
+func _on_increase_hp_cooldown_timeout():
+	IS_AVAILABLE = true
