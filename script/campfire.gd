967f1515c088892beb26dfebb22a4d96c4a7ea99
Can Vural <66164392+Can-Vural@users.noreply.github.com>
2025-05-30 15:32:30 +0300

    add decrease_hp method

--- script/campfire.gd
+++ script/campfire.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
-var IS_PLAYER_IN_AREA = false
-var IS_AVAILABLE = true
+var IS_PLAYER_IN_INCREASE_AREA = false
+var IS_PLAYER_IN_DECREASE_AREA = false
+
+var IS_INCREASE_AVAILABLE = true
+var IS_DECREASE_AVAILABLE = true
 
 
 func _ready():
@@ -9,24 +12,47 @@ func _ready():
 
 func _physics_process(delta):
 	increase_player_hp()
+	decrease_player_hp()
 
 
 func _on_hitbox_body_entered(body):
 	if body.has_method("player"):
-		IS_PLAYER_IN_AREA = true
+		IS_PLAYER_IN_INCREASE_AREA = true
 
 func _on_hitbox_body_exited(body):
 	if body.has_method("player"):
-		IS_PLAYER_IN_AREA = false
+		IS_PLAYER_IN_INCREASE_AREA = false
 
 func increase_player_hp():
-	if IS_PLAYER_IN_AREA:
-		if IS_AVAILABLE:
+	if IS_PLAYER_IN_INCREASE_AREA and not IS_PLAYER_IN_DECREASE_AREA:
+		if IS_INCREASE_AVAILABLE:
 			global.PLAYER_HEALTH += 15
 			
 			$increase_hp_cooldown.start()
-			IS_AVAILABLE = false
+			IS_INCREASE_AVAILABLE = false
+
+func decrease_player_hp():
+	if IS_PLAYER_IN_DECREASE_AREA:
+		if IS_DECREASE_AVAILABLE:
+			global.PLAYER_HEALTH -= 25
+			
+			$decrease_hp_cooldown.start()
+			IS_DECREASE_AVAILABLE = false
 
 
 func _on_increase_hp_cooldown_timeout():
-	IS_AVAILABLE = true
+	IS_INCREASE_AVAILABLE = true
+
+
+func _on_decrease_hitbox_body_entered(body):
+	if body.has_method("player"):
+		IS_PLAYER_IN_DECREASE_AREA = true
+
+
+func _on_decrease_hitbox_body_exited(body):
+	if body.has_method("player"):
+		IS_PLAYER_IN_DECREASE_AREA = false
+
+
+func _on_decrease_hp_cooldown_timeout():
+	IS_DECREASE_AVAILABLE = true
